b0d1dfbd3d1bd0e8c78f2205d940b07c679cc058
jlebiannic <jlebiannic@LT-SOD00006403.sodifrance.fr>
2020-01-24 12:07:15 +0100

    TUs

--- src/test/java/fr/mgen/editions/tests/IntegrationTest.java
+++ src/test/java/fr/mgen/editions/tests/IntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import fr.mgen.editions.RegroupeEditions;
 import fr.mgen.editions.dataset.DataSet;
+import fr.mgen.editions.util.SystemUtil;
 
 public class IntegrationTest {
 
@@ -23,6 +24,8 @@ public class IntegrationTest {
 	public void integrationTest() throws IOException, InterruptedException {
 		String integrationContentResult = clean(RegroupeEditions.regroupeEditions(dataset.getParams()));
 		String integrationContentExpected = clean(dataset.getIntegrationContentExpected());
+		SystemUtil.writeFileWithContent("d:\\tmpRes.txt", integrationContentResult);
+		SystemUtil.writeFileWithContent("d:\\tmpExpected.txt", integrationContentExpected);
 		assertEquals(integrationContentExpected, integrationContentResult);
 	}
 	
@@ -34,6 +37,7 @@ public class IntegrationTest {
 	}
 	
 	public String clean(String str) {
+		str = str.replaceAll("[\\r\\n]+", "");
 		str = str.replaceAll("date  de regroupement.+", "");
 		str = str.replaceAll("heure de regroupement.+", "");
 		str = str.replaceAll("date d'edition.+", "");
